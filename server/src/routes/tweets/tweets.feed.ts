ffd2fb5878cd02efa28bc686fb51110e71141c69
Archameuse <divulinm@gmail.com>
2026-06-28 21:30:06 +0300

    finished tweets feed

--- server/src/routes/tweets/tweets.feed.ts
+++ server/src/routes/tweets/tweets.feed.ts
@@ -16,6 +16,9 @@ import {
   desc,
   eq,
   isNotNull,
+  like,
+  ne,
+  or,
   SelectedFields,
   sql,
 } from "drizzle-orm";
@@ -23,6 +26,16 @@ import { alias, SQLiteSelect, union } from "drizzle-orm/sqlite-core";
 import { Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { z } from "zod";
+import {
+  dbTrendsToGlobalTrendsSchema,
+  dbTweetToGlobalTweetSchema,
+  FILTER,
+  FilterInput,
+  filterQuerySchema,
+  ORDER,
+  PaginationInput,
+  paginationQuerySchema,
+} from "./tweets.schema.js";
 
 const app = new Hono();
 
@@ -34,112 +47,6 @@ const userId = 1; //i would need to somehow get user id from session or somethin
 // tweets, <- profile | only tweets of this user
 // tweets&replies, <- profile | tweets of this user + replies to him
 // likes <- profile | only tweets this user liked
-enum FILTER {
-  media = "media",
-  likes = "likes",
-  tweets = "tweets",
-  tweetsAndReplies = "tweetsAndReplies",
-}
-
-enum ORDER {
-  top = "top", //by likes desc
-  new = "latest", // by created_at desc
-  old = "old", //by created_at asc
-}
-
-const DEFAULT_PAGE_LIMIT = 15;
-const MAX_PAGE_LIMIT = 25;
-const MIN_PAGE_LIMIT = 1;
-
-const dbTweetSchema = z.object({
-  tweet_id: z.number(),
-  content: z.string(),
-  image: z.string().nullable().optional(),
-  created_at: z.coerce.date(),
-  only_followers: z.coerce.boolean(),
-  author: z.object({
-    user_id: z.number(),
-    username: z.string(),
-    avatar: z.string().nullable().optional(),
-    is_followed: z.coerce.boolean(),
-  }),
-  reply_to: z.preprocess(
-    (val) => {
-      if (typeof val === "string") {
-        try {
-          return JSON.parse(val);
-        } catch {
-          return null;
-        }
-      } else return null;
-    },
-    z
-      .object({
-        tweet_id: z.number(),
-        username: z.string(),
-      })
-      .nullable()
-      .optional(),
-  ),
-
-  likes_count: z.coerce.number().default(0),
-  saves_count: z.coerce.number().default(0),
-  retweets_count: z.coerce.number().default(0),
-  replies_count: z.coerce.number().default(0),
-  retweeted_by: z.string().nullable().optional(),
-  hashtag: z.string().nullable().optional(),
-  is_liked: z.coerce.boolean(),
-  is_saved: z.coerce.boolean(),
-  is_retweeted: z.coerce.boolean(),
-});
-
-const dbTweetToGlobalTweetSchema = dbTweetSchema
-  .transform(
-    (db): Tweet => ({
-      id: db.tweet_id,
-      author: {
-        id: db.author.user_id,
-        username: db.author.username,
-        avatar: db.author.avatar,
-        followed: db.author.is_followed,
-      },
-      content: db.content,
-      created_at: db.created_at,
-      likes: db.likes_count,
-      replies: db.replies_count,
-      retweets: db.retweets_count,
-      saves: db.saves_count,
-      hashtag: db.hashtag,
-      image: db.image,
-      liked: db.is_liked,
-      saved: db.is_saved,
-      retweeted: db.is_retweeted,
-      onlyFollowers: db.only_followers,
-      replyTo: db.reply_to
-        ? {
-            id: db.reply_to.tweet_id,
-            username: db.reply_to.username,
-          }
-        : null,
-      retweetedBy: db.retweeted_by,
-    }),
-  )
-  .array();
-
-const paginationQuerySchema = z.object({
-  page: z.coerce.number().int().positive().min(1).catch(1),
-  perPage: z.coerce
-    .number()
-    .int()
-    .positive()
-    .min(MIN_PAGE_LIMIT)
-    .max(MAX_PAGE_LIMIT)
-    .catch(DEFAULT_PAGE_LIMIT),
-  // orderBy: z.enum(ORDER).catch(ORDER.new),
-  orderBy: z.preprocess((val) => val, z.enum(ORDER)).catch(ORDER.new),
-});
-
-type PaginationInput = z.input<typeof paginationQuerySchema>;
 
 const buildTweetQuery = (fetchRetweets?: boolean) => {
   const parent = alias(tweets, "reply_to");
@@ -155,12 +62,14 @@ const buildTweetQuery = (fetchRetweets?: boolean) => {
     author: {
       user_id: users.user_id,
       username: users.username,
-      is_followed: userId
+      is_followed: (userId
         ? sql<boolean>`EXISTS(SELECT 1 FROM ${follows} WHERE ${follows.follower_id} = ${userId} AND ${follows.followed_id} = ${tweets.user_id})`
-        : sql<boolean>`FALSE`,
+        : sql<boolean>`FALSE`
+      ).as("is_followed"),
       avatar: users.avatar,
     },
-    reply_to: sql<string | null>`
+    reply_to: tweets.reply_to,
+    reply_to_author: sql<string | null>`
       CASE
         WHEN ${tweets.reply_to} IS NOT NULL
           THEN json_object(
@@ -169,16 +78,19 @@ const buildTweetQuery = (fetchRetweets?: boolean) => {
           )
           ELSE NULL
       END
-      `,
-    is_liked: userId
+      `.as("reply_to_author"),
+    is_liked: (userId
       ? sql<boolean>`EXISTS(SELECT 1 FROM ${likes} WHERE ${likes.tweet_id} = ${tweets.tweet_id} AND ${likes.user_id} = ${userId})`
-      : sql<boolean>`FALSE`,
-    is_retweeted: userId
+      : sql<boolean>`FALSE`
+    ).as("is_liked"),
+    is_retweeted: (userId
       ? sql<boolean>`EXISTS(SELECT 1 FROM ${retweets} WHERE ${retweets.tweet_id} = ${tweets.tweet_id} AND ${retweets.user_id} = ${userId})`
-      : sql<boolean>`FALSE`,
-    is_saved: userId
+      : sql<boolean>`FALSE`
+    ).as("is_retweeted"),
+    is_saved: (userId
       ? sql<boolean>`EXISTS(SELECT 1 FROM ${saves} WHERE ${saves.tweet_id} = ${tweets.tweet_id} AND ${saves.user_id} = ${userId})`
-      : sql<boolean>`FALSE`,
+      : sql<boolean>`FALSE`
+    ).as("is_saved"),
     likes_count:
       sql<number>`(SELECT COUNT(*) FROM ${likes} WHERE ${likes.tweet_id} = ${tweets.tweet_id})`.as(
         "likes_count",
@@ -195,10 +107,10 @@ const buildTweetQuery = (fetchRetweets?: boolean) => {
       sql<number>`(SELECT COUNT(*) FROM ${tweets} ${replyCounter} WHERE ${replyCounter.reply_to} = ${tweets.tweet_id})`.as(
         "replies_count",
       ),
-    // likes_count: tweets.likes_count,
-    // retweets_count: tweets.retweets_count,
-    // saves_count: tweets.saves_count,
-    // replies_count: tweets.replies_count,
+    // likes_count: tweets.likes_count.as('likes_count'),
+    // retweets_count: tweets.retweets_count.as('retweets_count'),
+    // saves_count: tweets.saves_count.as('saves_count'),
+    // replies_count: tweets.replies_count.as('replies_count'),
     hashtag: sql<any>`(
             SELECT (${hashtags.hashtag}) 
             FROM ${tweets_hashtags} 
@@ -242,7 +154,7 @@ const pagination = <T extends SQLiteSelect>(
   return db
     .orderBy(
       orderBy === ORDER.top
-        ? desc(tweets.likes_count)
+        ? desc(sql`likes_count`)
         : orderBy === ORDER.old
           ? asc(tweets.created_at)
           : desc(tweets.created_at),
@@ -251,41 +163,175 @@ const pagination = <T extends SQLiteSelect>(
     .offset((page - 1) * perPage);
 };
 
-const filters = <T extends SQLiteSelect>(db: T) => {};
+const filters = <T extends SQLiteSelect>(
+  db: T,
+  query: Partial<FilterInput> = {},
+) => {
+  const { activeFilters, search, profileId, isRetweet, tweetId } =
+    filterQuerySchema.parse(query);
+  const parent = alias(tweets, "reply_to");
+  return db.where(
+    and(
+      like(tweets.content, `%${search}%`), //search filter
+      activeFilters.has(FILTER.media) //media filter
+        ? and(isNotNull(tweets.image), ne(tweets.image, ""))
+        : sql`TRUE`,
+      activeFilters.has(FILTER.likes) && profileId
+        ? sql`
+        EXISTS (
+          SELECT 1 
+          FROM ${likes} 
+          WHERE ${likes.tweet_id} = ${tweets.tweet_id} 
+          AND ${likes.user_id} = ${profileId})
+      `
+        : sql`TRUE`, //is profileId user liked this tweet filter
+      activeFilters.has(FILTER.profileTweets) && profileId // profileId user own tweets or retweets
+        ? isRetweet
+          ? eq(retweets.user_id, profileId)
+          : eq(tweets.user_id, profileId)
+        : sql`TRUE`,
+      activeFilters.has(FILTER.profileReplies) && profileId // replies to profileId user tweets cant work with retweet
+        ? isRetweet
+          ? sql`FALSE`
+          : sql`
+        EXISTS (
+          SELECT 1 
+            FROM ${tweets} ${parent} 
+            WHERE ${parent.tweet_id} = ${tweets.reply_to}
+            AND ${parent.user_id} = ${profileId}  
+        )
+        `
+        : sql`TRUE`,
+      activeFilters.has(FILTER.tweetReplies) && tweetId
+        ? isRetweet
+          ? sql`FALSE`
+          : eq(tweets.reply_to, tweetId)
+        : sql`TRUE`,
+    ),
+  );
+};
 
 // saved on /bookmarks + filters
 app.get("/bookmarks", async (c) => {
-  const { page, filter, limit } = c.req.query();
-  const start = performance.now();
+  const { page, scope, limit } = c.req.query();
   const data: Tweet[] = dbTweetToGlobalTweetSchema.parse(
     await pagination(
-      buildTweetQuery().innerJoin(
-        saves,
-        and(eq(tweets.tweet_id, saves.tweet_id), eq(saves.user_id, userId)),
+      filters(
+        buildTweetQuery().innerJoin(
+          saves,
+          and(eq(tweets.tweet_id, saves.tweet_id), eq(saves.user_id, userId)),
+        ),
+        { activeFilters: new Set([scope]) },
       ),
-      { page, perPage: limit },
+      { page, perPage: limit, orderBy: scope },
     ),
-  );
-  const end = performance.now();
-  // console.log(`Execution time ${(end - start).toFixed(2)} ms`);
-  return c.json(data);
-});
-
-app.get("/", async (c) => {
-  const { page, filter, limit } = c.req.query();
-  const data = dbTweetToGlobalTweetSchema.parse(
-    await pagination(buildTweetQuery(), { page: 1, orderBy: filter }),
   );
   return c.json(data);
 });
 
 // all w/o retweets on /explore (replies shown as normal tweets but with some marking that they are replies in fact) + filters+search
+app.get("/explore", async (c) => {
+  const { page, scope, limit, q } = c.req.query();
 
-// home on / own tweets+retweets + follows tweets and retweets + filters
+  const data = dbTweetToGlobalTweetSchema.parse(
+    await pagination(
+      filters(buildTweetQuery(), {
+        activeFilters: new Set([scope]),
+        search: q,
+      }),
+      {
+        page,
+        perPage: limit,
+        orderBy: scope,
+      },
+    ),
+  );
+  return c.json(data);
+});
 
-// profile on /user/:id users tweets+retweets + filters
+// profile on /user/:id and / users tweets+retweets + filters
+app.get("/user", async (c) => {
+  const { page, scope, limit, id } = c.req.query();
+  const processedScope = z
+    .preprocess(
+      (val) => val,
+      z.enum([FILTER.profileTweets, FILTER.profileReplies, FILTER.media]),
+    )
+    .catch(FILTER.profileTweets)
+    .parse(scope);
+  const processedId =
+    z.coerce
+      .number()
+      .int()
+      .min(1)
+      .optional()
+      .refine((val) => val || userId, {
+        error: 'You need to get authenticated to view "home" feed',
+      })
+      .parse(id) ?? userId;
+  // if no id passed in query then fallback to current logged in user or crash
+  const activeFilters = new Set([FILTER.profileTweets, processedScope]);
+  const data = dbTweetToGlobalTweetSchema.parse(
+    await pagination(
+      activeFilters.has(FILTER.profileReplies)
+        ? filters(buildTweetQuery(false), {
+            profileId: processedId,
+            activeFilters,
+          })
+        : union(
+            filters(buildTweetQuery(false), {
+              profileId: processedId,
+              activeFilters,
+            }),
+            filters(buildTweetQuery(true), {
+              profileId: processedId,
+              activeFilters,
+              isRetweet: true,
+            }),
+          ).$dynamic(),
+      { page, perPage: limit },
+    ),
+  );
+  return c.json(data);
+});
 
 // replies /replies/:id list of tweets that are replying to this specific id open through modal
+app.get("/replies/:id", async (c) => {
+  const { id } = c.req.param();
+  const { page, limit } = c.req.query();
+  const processedId = z.coerce.number().int().min(1).parse(id);
+  const data = dbTweetToGlobalTweetSchema.parse(
+    await pagination(
+      filters(buildTweetQuery(), {
+        tweetId: processedId,
+        activeFilters: new Set([FILTER.tweetReplies]),
+      }),
+      { page, perPage: limit },
+    ),
+  );
+  return c.json(data);
+});
 
 // trends /trends top5 hashtags with most tweets for now, maybe later will add some logic
+app.get("/trends", async (c) => {
+  const tweetsCounter = alias(tweets_hashtags, "tweets_counter");
+  const data = dbTrendsToGlobalTrendsSchema.parse(
+    await db
+      .select({
+        hashtag_id: hashtags.hashtag_id,
+        hashtag: hashtags.hashtag,
+        tweets_count: count(tweets_hashtags.hashtag_id).as("tweets_count"),
+        // tweets_counts: hashtags.tweets_count <- for scaling
+      })
+      .from(hashtags)
+      .leftJoin(
+        tweets_hashtags,
+        eq(hashtags.hashtag_id, tweets_hashtags.hashtag_id),
+      )
+      .groupBy(hashtags.hashtag_id, hashtags.hashtag)
+      .orderBy(desc(sql`tweets_count`))
+      .limit(5),
+  );
+  return c.json(data);
+});
 export default app;
---
+++ server/src/routes/tweets/tweets.schema.ts
@@ -0,0 +1,145 @@
+import z from "zod";
+
+const DEFAULT_PAGE_LIMIT = 10;
+const MAX_PAGE_LIMIT = 25;
+const MIN_PAGE_LIMIT = 1;
+
+export enum FILTER {
+  all = "all",
+  media = "media",
+  likes = "likes",
+  profileTweets = "tweets",
+  profileReplies = "replies",
+  tweetReplies = "tweet-replies",
+}
+
+export enum ORDER {
+  top = "top", //by likes desc
+  new = "latest", // by created_at desc
+  old = "old", //by created_at asc
+}
+
+export const dbTweetSchema = z.object({
+  tweet_id: z.number(),
+  content: z.string(),
+  image: z.string().nullable().optional(),
+  created_at: z.preprocess((val) => {
+    if (typeof val === "number" && !Number.isNaN(val)) {
+      return new Date(val * 1000);
+    } else if (typeof val === "string") {
+      return new Date(val);
+    }
+    return val;
+  }, z.date()),
+  only_followers: z.coerce.boolean(),
+  author: z.object({
+    user_id: z.number(),
+    username: z.string(),
+    avatar: z.string().nullable().optional(),
+    is_followed: z.coerce.boolean(),
+  }),
+  reply_to: z.number().nullable().optional(),
+  reply_to_author: z.preprocess(
+    (val) => {
+      if (typeof val === "string") {
+        try {
+          return JSON.parse(val);
+        } catch {
+          return null;
+        }
+      } else return null;
+    },
+    z
+      .object({
+        tweet_id: z.number(),
+        username: z.string(),
+      })
+      .nullable()
+      .optional(),
+  ),
+
+  likes_count: z.coerce.number().default(0),
+  saves_count: z.coerce.number().default(0),
+  retweets_count: z.coerce.number().default(0),
+  replies_count: z.coerce.number().default(0),
+  retweeted_by: z.string().nullable().optional(),
+  hashtag: z.string().nullable().optional(),
+  is_liked: z.coerce.boolean(),
+  is_saved: z.coerce.boolean(),
+  is_retweeted: z.coerce.boolean(),
+});
+
+export const dbTweetToGlobalTweetSchema = dbTweetSchema
+  .transform(
+    (db): Tweet => ({
+      id: db.tweet_id,
+      author: {
+        id: db.author.user_id,
+        username: db.author.username,
+        avatar: db.author.avatar,
+        followed: db.author.is_followed,
+      },
+      content: db.content,
+      created_at: db.created_at,
+      likes: db.likes_count,
+      replies: db.replies_count,
+      retweets: db.retweets_count,
+      saves: db.saves_count,
+      hashtag: db.hashtag,
+      image: db.image,
+      liked: db.is_liked,
+      saved: db.is_saved,
+      retweeted: db.is_retweeted,
+      onlyFollowers: db.only_followers,
+      replyTo: db.reply_to_author
+        ? {
+            id: db.reply_to_author.tweet_id,
+            username: db.reply_to_author.username,
+          }
+        : null,
+      retweetedBy: db.retweeted_by,
+    }),
+  )
+  .array();
+
+export const paginationQuerySchema = z.object({
+  page: z.coerce.number().int().positive().min(1).catch(1),
+  perPage: z.coerce
+    .number()
+    .int()
+    .positive()
+    .min(MIN_PAGE_LIMIT)
+    .max(MAX_PAGE_LIMIT)
+    .catch(DEFAULT_PAGE_LIMIT),
+  // orderBy: z.enum(ORDER).catch(ORDER.new),
+  orderBy: z.preprocess((val) => val, z.enum(ORDER)).catch(ORDER.new),
+});
+export const filterQuerySchema = z.object({
+  //   filter: z.preprocess((val) => val, z.enum(FILTER)).catch(FILTER.all),
+  activeFilters: z
+    .preprocess((val) => val, z.set(z.enum(FILTER)))
+    .catch(new Set([FILTER.all])),
+  search: z.string().trim().toLowerCase().catch(""),
+  profileId: z.coerce.number().int().positive().optional(),
+  isRetweet: z.coerce.boolean().catch(false),
+  tweetId: z.coerce.number().int().positive().optional(),
+});
+
+export const dbTrendsSchema = z.object({
+  hashtag_id: z.number(),
+  hashtag: z.string(),
+  tweets_count: z.number(),
+});
+
+export const dbTrendsToGlobalTrendsSchema = dbTrendsSchema
+  .transform(
+    (db): Trend => ({
+      id: db.hashtag_id,
+      hashtag: db.hashtag,
+      tweets: db.tweets_count,
+    }),
+  )
+  .array();
+
+export type PaginationInput = z.input<typeof paginationQuerySchema>;
+export type FilterInput = z.input<typeof filterQuerySchema>;
